2162fe341b8a1c696de2ca6027e9d13f425e8e9c
Alexandre Costa <alexandresantoscosta@gmail.com>
2024-09-21 07:28:48 -0300

    Adicionada soma

--- MinhaCalculadora/Program.cs
+++ MinhaCalculadora/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ * Quero poder realizar as quatro operações matemáticas básicas (somar, subtrair, multiplicar e dividir) números inteiros para que eu possa estudar melhor.
+ */
+
+Console.WriteLine("Informe o primeiro número: ");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Informe o segundo número: ");
+int y = int.Parse(Console.ReadLine());
+
+
+Console.WriteLine($"A soma de {x} + {y} = {x + y}");
