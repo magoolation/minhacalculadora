5a567a36181d69b628cc75615d8bb7c7efd2b3fc
Alexandre Costa <alexandresantoscosta@gmail.com>
2024-09-21 12:14:44 -0300

    Adiciona classe Calculadora

---
+++ MinhaCalculadora/Calculadora.cs
@@ -0,0 +1,12 @@
+﻿namespace MinhaCalculadora;
+
+public static class Calculadora
+{
+    public static int Somar(int x, int y) => x + y;
+
+    public static int Subtrair(int x, int y) => x - y;
+
+public static int Multiplicar(int x, int y) => x * y;
+
+    public static int Dividir(int x, int y) => x / y;
+}
--- MinhaCalculadora/Program.cs
+++ MinhaCalculadora/Program.cs
@@ -2,6 +2,8 @@
  * Quero poder realizar as quatro operações matemáticas básicas (somar, subtrair, multiplicar e dividir) números inteiros para que eu possa estudar melhor.
  */
 
+using MinhaCalculadora;
+
 Console.WriteLine("Informe o primeiro número: ");
 int x = int.Parse(Console.ReadLine());
 Console.WriteLine("Informe o segundo número: ");
@@ -10,11 +12,11 @@ Console.WriteLine("Qual a operação: ");
 string operacao = Console.ReadLine();
 
 if (operacao == "+")
-    Console.WriteLine($"A soma de {x} + {y} = {x + y}");
+    Console.WriteLine($"A soma de {x} + {y} = {Calculadora.Somar(x,y)}");
 else if (operacao == "-")
-    Console.WriteLine($"A diferença de {x} - {y} = {x - y}");
+    Console.WriteLine($"A diferença de {x} - {y} = {Calculadora.Subtrair(x,y)}");
 else if (operacao == "*")
-    Console.WriteLine($"O produto de {x} * {y} = {x * y}");
+    Console.WriteLine($"O produto de {x} * {y} = {Calculadora.Multiplicar(x,y)}");
 else if (operacao == "/")
 {
     if (y == 0)
@@ -22,7 +24,7 @@ else if (operacao == "/")
         Console.WriteLine("Erro de divisão por zero.");
     }
     else
-        Console.WriteLine($"A divisÃo de {x} / {y} = {x / y}");
+        Console.WriteLine($"A divisÃo de {x} / {y} = {Calculadora.Dividir(x,y)}");
 }
 else
     Console.WriteLine($"Operação {operacao} é inválida.");
